ef999441a4e8a57211245841cb12c2f62c1343ff
PallaviVanarse <59473097+PallaviVanarse@users.noreply.github.com>
2024-11-26 22:50:49 +0530

    #added required db context

---
+++ SampleMicroservice/Data/ProductDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductMicroservice.Models;
+
+namespace ProductMicroservice.Data;
+
+public class ProductDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options) { }
+}
